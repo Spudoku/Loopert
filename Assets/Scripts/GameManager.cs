39ad1c0c39309c8af8a3ffc51238865b6622eba9
Spudoku <calebisntshort@gmail.com>
2025-08-01 16:53:25 -0700

    preparing pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,39 @@
 using System.Collections;
+using System.Runtime.CompilerServices;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private static GameObject menu;
+    static bool isPaused = true;
     public static void StartSequence()
     {
         // show tutorial UI
+        SetMenu(true);
+    }
+
+    public static void ToggleMenu()
+    {
+        isPaused = !isPaused;
+        SetMenu(isPaused);
+    }
+
+    private static void SetMenu(bool paused)
+    {
+        menu.SetActive(paused);
+    }
+
+    private void Update()
+    {
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
     }
 
     public static IEnumerator EndSequence()
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -2,5 +2,6 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+
     // Handle the tutorial/pause UI
 }
